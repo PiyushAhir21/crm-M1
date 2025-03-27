9cf47921a2a45f8efd158a3df97794f5f9b931fa
PiyushAhir21 <vasudevahir21@gmail.com>
2025-03-27 14:57:09 +0530

    Data Provider

--- vtiger-crm-M1/src/test/java/extra/Demo3.java
+++ vtiger-crm-M1/src/test/java/extra/Demo3.java
@@ -1,12 +1,15 @@
 package extra;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Demo3 {
-	@Test(groups = "RT")
-	public void firefox() {
-		WebDriver driver = new FirefoxDriver();
+	@Test(invocationCount = 20 , threadPoolSize = 15)
+	public void firefox() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.close();
+		Thread.sleep(5000);
 	}
 }
--- vtiger-crm-M1/src/test/java/extra/Demo4.java
+++ vtiger-crm-M1/src/test/java/extra/Demo4.java
@@ -1,18 +1,83 @@
 package extra;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import base_utility.BaseClass;
+public class Demo4 {
 
-public class Demo4 extends BaseClass{
-
-	@Test
-	public void demo1() {
-		System.out.println("Creating Opp with Organization name");
+	@Test(dataProvider = "getPlaces")
+	public void HomeTown(String city, String village) {
+//		String city = "Noida";
+//		String village = "Ramgarh";
+		System.out.println("I'm staying in " + city + " and " + village);
 	}
 
-	@Test
-	public void demo2() {
-		System.out.println("Creating Opp with Type");
+	@DataProvider
+	public Object[][] getPlaces() throws EncryptedDocumentException, IOException {
+		Object[][] places = new Object[9][2];
+
+		FileInputStream fis = new FileInputStream("C:\\Users\\User\\Desktop\\testData1.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sh = wb.getSheet("DP");
+		for (int i = 1; i < 5; i++) {
+			String data = sh.getRow(i).getCell(0).getStringCellValue();
+			for (int j = 1; j < 5; j++) {
+				places[i][0] = data;
+			}
+		}
+//		String city1 = sh.getRow(1).getCell(0).getStringCellValue();
+//		String city2 = sh.getRow(2).getCell(0).getStringCellValue();
+//		String city3 = sh.getRow(3).getCell(0).getStringCellValue();
+//		String city4 = sh.getRow(4).getCell(0).getStringCellValue();
+
+		places[0][1] = "Ramgarh";
+
+		places[1][1] = "Karnal";
+
+		places[2][1] = "Sonebhadra";
+
+		places[3][1] = "U.K.";
+
+		places[4][0] = "Noida";
+		places[4][1] = "Madhupur";
+
+		places[5][0] = "Delhi";
+		places[5][1] = "Muradabad";
+
+		places[6][0] = "Ghaziabad";
+		places[6][1] = "Patna";
+
+		places[7][0] = "Ballia";
+		places[7][1] = "Delhi";
+
+		places[8][0] = "Noida";
+		places[8][1] = "Azamgarh";
+		return places;
 	}
+
+	public void cities() throws EncryptedDocumentException, IOException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\User\\Desktop\\testData1.xlsx");
+
+		Workbook wb = WorkbookFactory.create(fis);
+
+		Sheet sh = wb.getSheet("DP");
+		for (int i = 1; i < 5; i++) {
+			String data = sh.getRow(i).getCell(0).getStringCellValue();
+			System.out.println(data);
+		}
+
+		for (int i = 1; i < 5; i++) {
+			String data = sh.getRow(i).getCell(1).getStringCellValue();
+			System.out.println(data);
+		}
+
+	}
+
 }
